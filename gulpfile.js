2f0c32aafc4d5e59eb3be90dc3c68fdb3cf07766
ekaterina-gasevskaia <ekaterina.gasevskaia@gmail.com>
2021-05-23 16:57:22 +0200

    Подготовка. Добавление сборщика.

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+// Определяем константы Gulp
+const { src, dest, parallel, series, watch } = require('gulp');
+// Подключаем Browsersync
+const browserSync = require('browser-sync').create();
+// Подключаем gulp-concat
+const concat = require('gulp-concat');
+// Подключаем gulp-uglify-es
+const uglify = require('gulp-uglify-es').default;
+
+
+// Определяем логику работы Browsersync
+function browsersync() {
+    browserSync.init({ // Инициализация Browsersync
+        server: { baseDir: 'app/' }, // Указываем папку сервера
+        notify: false, // Отключаем уведомления
+        online: true // Режим работы: true или false
+    })
+}
+
+// Экспортируем функцию browsersync() как таск browsersync. Значение после знака = это имеющаяся функция.
+exports.browsersync = browsersync;
+
+
+
+function scripts() {
+    return src([ // Берём файлы из источников
+        'node_modules/jquery/dist/jquery.min.js', // Пример подключения библиотеки
+        'app/js/app.js', // Пользовательские скрипты, использующие библиотеку, должны быть подключены в конце
+    ])
+        .pipe(concat('app.min.js')) // Конкатенируем в один файл
+        .pipe(uglify()) // Сжимаем JavaScript
+        .pipe(dest('app/js/')) // Выгружаем готовый файл в папку назначения
+        .pipe(browserSync.stream()) // Триггерим Browsersync для обновления страницы
+}
